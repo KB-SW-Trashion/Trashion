949582b839b8ce3e2298e01e76577283444224ed
Chungjiyoon <cjy0565@kookmin.ac.kr>
2022-08-01 22:12:31 +0900

    feat:navbarlink

--- trashion_front/src/components/Navbar/Navbar.js
+++ trashion_front/src/components/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Navbar.css';
 import logo from './Trashion_logo.png';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <>
       <div className="logobox">
         <div className="buttonbox">
-          <a href="/">
+          <Link to="/">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" className="humbleicons hi-chats">
               <path
                 xmlns="http://www.w3.org/2000/svg"
@@ -28,8 +29,8 @@ export default function Navbar() {
               <circle xmlns="http://www.w3.org/2000/svg" r="1" fill="currentColor" transform="matrix(-1 0 0 1 10 9.5)" />
               <circle xmlns="http://www.w3.org/2000/svg" r="1" fill="currentColor" transform="matrix(-1 0 0 1 7 9.5)" />
             </svg>
-          </a>
-          <a href="/">
+          </Link>
+          <Link to="/">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" className="humbleicons hi-pencil">
               <path
                 xmlns="http://www.w3.org/2000/svg"
@@ -40,8 +41,8 @@ export default function Navbar() {
                 d="M13.5 7.5l3 3M4 20v-3.5L15.293 5.207a1 1 0 011.414 0l2.086 2.086a1 1 0 010 1.414L7.5 20H4z"
               />
             </svg>
-          </a>
-          <a href="/">
+          </Link>
+          <Link to="/">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" className="humbleicons hi-heart">
               <path
                 xmlns="http://www.w3.org/2000/svg"
@@ -51,8 +52,8 @@ export default function Navbar() {
                 d="M12 8c-1.574-1.635-4.46-2.135-6.035-.5-1.573 1.635-1.34 3.836 0 5.752C7.306 15.168 9.41 16.89 12 19c2.59-2.11 4.694-3.832 6.035-5.748 1.34-1.916 1.573-4.117 0-5.752C16.46 5.865 13.574 6.365 12 8z"
               />
             </svg>
-          </a>
-          <a href="/">
+          </Link>
+          <Link to="/">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" className="humbleicons hi-user">
               <path
                 xmlns="http://www.w3.org/2000/svg"
@@ -63,7 +64,7 @@ export default function Navbar() {
                 d="M18 19v-1.25c0-2.071-1.919-3.75-4.286-3.75h-3.428C7.919 14 6 15.679 6 17.75V19m9-11a3 3 0 11-6 0 3 3 0 016 0z"
               />
             </svg>
-          </a>
+          </Link>
         </div>
         <img className="logo" src={logo}></img>
       </div>
