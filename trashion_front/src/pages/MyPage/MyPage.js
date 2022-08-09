d37001cc3ab02a91f591b22316987dd2d4eef13c
DESKTOP-JEJ9DKA\User <cjy0565@kookmin.ac.kr>
2022-08-10 01:58:20 +0900

    feat:detail

--- trashion_front/src/pages/MyPage/MyPage.js
+++ trashion_front/src/pages/MyPage/MyPage.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Navbar, Footer } from 'components';
 import img_example from '../../img/img_example.jpg';
-
-import './MyPage.module.css';
+import styles from './MyPage.module.css';
 
 export default function MyPage() {
   return (
     <div>
       <Navbar />
 
-      <div className="MyPage_bodybox">
-        <div className="MyPage_bodyleft">
-          <img className="productImg" src={img_example} />
+      <div className={styles.MyPage_bodybox}>
+        <div className={styles.MyPage_bodyleft}>
+          <img className={styles.productImg} src={img_example} width="300px" height="300px" />
         </div>
-        <div className="MyPage_bodyright">
+        <div className={styles.MyPage_bodyright}>
           <p>이름:나는지윤이다 이거는 예시</p>
           <p>사이즈:나는 이거이거이거이거다</p>
         </div>
       </div>
-      <div className="MyPage_list">
-        <p>내가 쓴 글</p>
+      <div className={styles.MyPage_list}>
+        <p className={styles.MyPage_list_title}>내가 쓴 글</p>
+        <div></div>
       </div>
 
-      <div className="MyPage_footerbox">
+      <div className={styles.MyPage_footerbox}>
         <Footer />
       </div>
     </div>
