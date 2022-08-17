3b97eb125727507828327d2c8edddfc785864b0a
mumat0103 <wsx2138@naver.com>
2022-08-17 17:08:27 +0900

    feat: Generic View를 활용한 유저 정보 수정

--- trashion_back/accounts/serializers.py
+++ trashion_back/accounts/serializers.py
@@ -41,14 +41,17 @@ class SignUpSerializer(RegisterSerializer):
         }
 
 class UserEditSerializer(serializers.ModelSerializer):
+    realname = serializers.ReadOnlyField()
+    email = serializers.ReadOnlyField()
+    
     class Meta:
         model = User
-        fields = ['nickname', 'address', 'phone']
+        fields = ['email', 'realname', 'nickname', 'address', 'phone']
         
-    def update(self, instance, validated_data):
-        instance.nickname = validated_data.get('nickname', instance.nickname)
-        instance.address = validated_data.get('address', instance.address)
-        instance.phone = validated_data.get('phone', instance.phone)
+    # def update(self, instance, validated_data):
+    #     instance.nickname = validated_data.get('nickname', instance.nickname)
+    #     instance.address = validated_data.get('address', instance.address)
+    #     instance.phone = validated_data.get('phone', instance.phone)
 
 # class SignUpSerializer(serializers.ModelSerializer):
 #     class Meta:
