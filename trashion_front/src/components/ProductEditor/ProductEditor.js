0a06abff218581e1807ef949d07375ba27a23243
zkxmdkdltm <zkxmdkdltm@gmail.com>
2022-08-21 01:17:19 +0900

    feat: 글쓰기 페이지 - 카테고리 추가 & css 수정

--- trashion_front/src/components/ProductEditor/ProductEditor.js
+++ trashion_front/src/components/ProductEditor/ProductEditor.js
@@ -10,6 +10,38 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import ListSubheader from '@mui/material/ListSubheader';
+import Select from '@mui/material/Select';
+import { pink } from '@mui/material/colors';
+import { styled } from '@mui/material';
+
+const radioSX = { '&.Mui-checked': { color: pink[200] } };
+const labelSX = { '&.Mui-focused': { color: pink[200] }, color: 'black' };
+// const inputSX = { '&.Mui-focused': { color: pink[200] } };
+const CssTextField = styled(TextField, {
+  shouldForwardProp: (props) => props !== 'focusColor',
+})((p) => ({
+  // input label when focused
+  '& label.Mui-focused': {
+    color: p.focusColor,
+  },
+  // focused color for input with variant='standard'
+  '& .MuiInput-underline:after': {
+    borderBottomColor: p.focusColor,
+  },
+  // focused color for input with variant='filled'
+  '& .MuiFilledInput-underline:after': {
+    borderBottomColor: p.focusColor,
+  },
+  // focused color for input with variant='outlined'
+  '& .MuiOutlinedInput-root': {
+    '&.Mui-focused fieldset': {
+      borderColor: p.focusColor,
+    },
+  },
+}));
 
 const ProductEditor = ({ isEdit, originData, isNew }) => {
   const titleRef = useRef();
@@ -25,6 +57,7 @@ const ProductEditor = ({ isEdit, originData, isNew }) => {
   const [condition, setCondition] = useState(''); //input
   const [category, setCategory] = useState(''); //select
   const [period, setPeriod] = useState(''); //input
+  const [post_type, setPostType] = useState(''); //radio
 
   const { onCreate, onEdit, onRemove } = useContext(ProductDispatchContext);
 
@@ -41,10 +74,10 @@ const ProductEditor = ({ isEdit, originData, isNew }) => {
 
     if (window.confirm(isEdit ? '글을 수정하시겠습니까?' : '새로운 글을 작성하시겠습니까?')) {
       if (!isEdit) {
-        onCreate(date, title, content, price, size, condition, category, period);
+        onCreate(date, title, content, price, size, condition, category, period, post_type);
         navigate('/', { replace: true });
       } else {
-        onEdit(originData.id, date, title, content, price, size, condition, category, period);
+        onEdit(originData.id, date, title, content, price, size, condition, category, period, post_type);
         navigate('/', { replace: true });
       }
     }
@@ -67,6 +100,7 @@ const ProductEditor = ({ isEdit, originData, isNew }) => {
       setCondition(originData.condition);
       setCategory(originData.category);
       setPeriod(originData.period);
+      setPostType(originData.post_type);
     }
   }, [isEdit, originData]);
 
@@ -87,41 +121,99 @@ const ProductEditor = ({ isEdit, originData, isNew }) => {
         />
         <div className={styles.input_wrap}>
           <section>
-            <div className={styles.input_box}>
-              <TextField ref={titleRef} value={title} onChange={(e) => setTitle(e.target.value)} required id="outlined-required" label="제목" variant="outlined" />
+            <FormControl sx={{ m: 1, minWidth: 200 }}>
+              <InputLabel htmlFor="grouped-select">카테고리</InputLabel>
+              <Select defaultValue="" id="grouped-select" label="Grouping" value={category} onChange={(e) => setCategory(e.target.value)}>
+                <ListSubheader>상의</ListSubheader>
+                <MenuItem value={'반팔 티셔츠'}>반팔 티셔츠</MenuItem>
+                <MenuItem value={'긴팔 티셔츠'}>긴팔 티셔츠</MenuItem>
+                <MenuItem value={'후드 티셔츠'}>후드 티셔츠</MenuItem>
+                <MenuItem value={'셔츠 / 블라우스'}>셔츠 / 블라우스</MenuItem>
+                <MenuItem value={'기타'}>기타</MenuItem>
+                <ListSubheader>하의</ListSubheader>
+                <MenuItem value={'청바지'}>청바지</MenuItem>
+                <MenuItem value={'반바지'}>반바지</MenuItem>
+                <MenuItem value={'슬랙스'}>슬랙스</MenuItem>
+                <MenuItem value={'트레이닝팬츠'}>트레이닝팬츠</MenuItem>
+                <MenuItem value={'기타'}>기타</MenuItem>
+                <ListSubheader>아우터</ListSubheader>
+                <MenuItem value={'후드집업'}>후드집업</MenuItem>
+                <MenuItem value={'코트'}>코트</MenuItem>
+                <MenuItem value={'가디건'}>가디건</MenuItem>
+                <MenuItem value={'패딩'}>패딩</MenuItem>
+                <MenuItem value={'기타'}>기타</MenuItem>
+                <ListSubheader>신발</ListSubheader>
+                <MenuItem value={'운동화'}>운동화</MenuItem>
+                <MenuItem value={'슬리퍼'}>슬리퍼</MenuItem>
+                <MenuItem value={'구두'}>구두</MenuItem>
+                <MenuItem value={'부츠'}>부츠</MenuItem>
+                <MenuItem value={'기타'}>기타</MenuItem>
+              </Select>
+            </FormControl>
+            <div className={styles.radio_box}>
+              <FormControl>
+                <FormLabel id="demo-row-radio-buttons-group-label" sx={labelSX}>
+                  게시물 타입
+                </FormLabel>
+                <RadioGroup row aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group" value={post_type} onChange={(e) => setPostType(e.target.value)}>
+                  <FormControlLabel value="product" control={<Radio sx={radioSX} />} label="옷" />
+                  <FormControlLabel value="style" control={<Radio sx={radioSX} />} label="스타일" />
+                </RadioGroup>
+              </FormControl>
             </div>
             <div className={styles.input_box}>
-              <textarea placeholder="Content" ref={contentRef} value={content} onChange={(e) => setContent(e.target.value)} />
+              <CssTextField ref={titleRef} value={title} onChange={(e) => setTitle(e.target.value)} focusColor="#f8bbd0" required id="outlined-required" label="제목" variant="outlined" />
+            </div>
+
+            <div className={styles.input_box}>
+              <CssTextField ref={priceRef} value={price} onChange={(e) => setPrice(e.target.value)} focusColor="#f8bbd0" required id="outlined-required" label="가격" variant="outlined" />
             </div>
             <div className={styles.input_box}>
-              <TextField ref={priceRef} value={price} onChange={(e) => setPrice(e.target.value)} required id="outlined-required" label="가격" variant="outlined" />
-            </div>
-            <div className={styles.input_box}>
-              <TextField ref={periodRef} value={period} onChange={(e) => setPeriod(e.target.value)} required id="outlined-required" label="착용기간" variant="outlined" />
+              <CssTextField ref={periodRef} value={period} onChange={(e) => setPeriod(e.target.value)} focusColor="#f8bbd0" required id="outlined-required" label="착용기간" variant="outlined" />
             </div>
             <div className={styles.radio_box}>
               <FormControl>
-                <FormLabel id="demo-row-radio-buttons-group-label">상품 컨디션</FormLabel>
+                <FormLabel id="demo-row-radio-buttons-group-label" sx={labelSX}>
+                  상품 컨디션
+                </FormLabel>
                 <RadioGroup row aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group" value={condition} onChange={(e) => setCondition(e.target.value)}>
-                  <FormControlLabel value="새상품(미개봉)" control={<Radio />} label="새상품(미개봉)" />
-                  <FormControlLabel value="거의 새상품" control={<Radio />} label="거의 새상품" />
-                  <FormControlLabel value="사용감 있는 깨끗한 상품" control={<Radio />} label="사용감 있는 깨끗한 상품" />
-                  <FormControlLabel value="사용 흔적이 많은 상품" control={<Radio />} label="사용 흔적이 많은 상품" />
+                  <FormControlLabel value="새상품(미개봉)" control={<Radio sx={radioSX} />} label="새상품(미개봉)" />
+                  <FormControlLabel value="거의 새상품" control={<Radio sx={radioSX} />} label="거의 새상품" />
+                  <FormControlLabel value="사용감 있는 깨끗한 상품" control={<Radio sx={radioSX} />} label="사용감 있는 깨끗한 상품" />
+                  <FormControlLabel value="사용 흔적이 많은 상품" control={<Radio sx={radioSX} />} label="사용 흔적이 많은 상품" />
                 </RadioGroup>
               </FormControl>
             </div>
             <div className={styles.radio_box}>
               <FormControl>
-                <FormLabel id="demo-row-radio-buttons-group-label">상품 사이즈</FormLabel>
+                <FormLabel id="demo-row-radio-buttons-group-label" sx={labelSX}>
+                  상품 사이즈
+                </FormLabel>
                 <RadioGroup row aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group" value={size} onChange={(e) => setSize(e.target.value)}>
-                  <FormControlLabel value="FREE" control={<Radio />} label="FREE" />
-                  <FormControlLabel value="S" control={<Radio />} label="S" />
-                  <FormControlLabel value="M" control={<Radio />} label="M" />
-                  <FormControlLabel value="L" control={<Radio />} label="L" />
-                  <FormControlLabel value="XL" control={<Radio />} label="XL" />
+                  <FormControlLabel value="free" control={<Radio sx={radioSX} />} label="FREE" />
+                  <FormControlLabel value="small" control={<Radio sx={radioSX} />} label="S" />
+                  <FormControlLabel value="medium" control={<Radio sx={radioSX} />} label="M" />
+                  <FormControlLabel value="large" control={<Radio sx={radioSX} />} label="L" />
+                  <FormControlLabel value="xlarge" control={<Radio sx={radioSX} />} label="XL" />
                 </RadioGroup>
               </FormControl>
             </div>
+            <div className={styles.input_box}>
+              <CssTextField
+                ref={contentRef}
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                multiline
+                minRows={20}
+                focusColor="#f8bbd0"
+                required
+                id="outlined-required"
+                label="내용"
+                variant="outlined"
+                style={{ width: '950px' }}
+              />
+            </div>
+
             <ImageUploader />
           </section>
         </div>
