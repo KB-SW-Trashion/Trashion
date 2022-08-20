5fa5e5ec4bb5d25bce19a840afcbd06184836eea
DESKTOP-JEJ9DKA\User <cjy0565@kookmin.ac.kr>
2022-08-20 11:34:27 +0900

    feat:Mypagedetail&design

--- trashion_front/src/components/index.js
+++ trashion_front/src/components/index.js
@@ -10,7 +10,7 @@ export { default as KakaoLoginButton } from './kakaoLoginButton/kakaoLoginButton
 export { default as Today_product } from './Today_product/Today_product';
 export { default as Product_img } from './Product_img/Product_img';
 export { default as Img_small } from './Img_small/Img_small';
-export { default as Toggle_button } from './Toggle_button/Toggle_button';
+// export { default as Toggle_button } from './Toggle_button/Toggle_button';
 export { default as Styles_img } from './Styles_img/Styles_img';
 export { default as Styles } from './Styles/Styles';
 export { default as Category } from './Category/Category';
