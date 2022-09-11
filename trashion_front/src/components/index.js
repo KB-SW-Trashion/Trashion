d73e3e76fefc7bc7964277f54942d71dc7424599
zkxmdkdltm <zkxmdkdltm@gmail.com>
2022-09-11 17:24:00 +0900

    refactor: api 이름 변경 및 사용하지 않는 component 제거

--- trashion_front/src/components/index.js
+++ trashion_front/src/components/index.js
@@ -5,9 +5,7 @@ export { default as ID } from './ID/ID';
 export { default as Password } from './Password/Password';
 export { default as Forgot } from './Forgot/Forgot';
 export { default as Footer } from './Footer/Footer';
-export { default as PersistentDrawerLeft } from './Drawer/Drawer';
 export { default as KakaoLoginButton } from './kakaoLoginButton/kakaoLoginButton';
-export { default as Today_product } from './Today_product/Today_product';
 export { default as Product_img } from './Product_img/Product_img';
 export { default as Img_small } from './Img_small/Img_small';
 
