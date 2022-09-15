99dfbc1f6a926b650ffbcfcf3aed30a4913aa704
DESKTOP-JEJ9DKA\User <cjy0565@kookmin.ac.kr>
2022-09-16 00:53:27 +0900

    feat:Link,fix:Linkerror

--- trashion_front/src/components/Forgot/Forgot.js
+++ trashion_front/src/components/Forgot/Forgot.js
@@ -5,7 +5,7 @@ import styles from './Fotgot.module.css';
 const Forgot = () => {
   return (
     <div className={styles.forgot_box}>
-      <Link to="/">
+      <Link to="/Findpassword">
         <span className={styles.forgot_find}>아이디 찾기&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;비밀번호 찾기</span>
       </Link>
       <span>&nbsp;&nbsp;&nbsp;|</span>
--- trashion_front/src/pages/Scrap_List/Scrap_List.js
+++ trashion_front/src/pages/Scrap_List/Scrap_List.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { List_Null_Scrap, Navbar, Scrap_product } from 'components';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import user from 'api/userInfo';
+import { userInfoState, authState, reviewState } from 'store';
 
-export default function Scrap_List(props) {
+export default function Scrap_List() {
+  const userAuth = useRecoilValue(authState);
+  const [userInfo, setUserInfo] = useRecoilState(userInfoState);
+  const user_id = userAuth.user_id;
+
+  useEffect(() => {
+    user.getUserInfo(user_id).then((res) => {
+      setUserInfo({
+        like_item_count: res.data.like_item_count,
+      });
+      if (res.data.social_profile) {
+        setUserInfo((userInfo) => ({ ...userInfo, social_profile: res.data.social_profile }));
+      } else if (res.data.profile_image) {
+        setUserInfo((userInfo) => ({ ...userInfo, profile_image: res.data.profile_image.photo }));
+      }
+    });
+  }, []);
+
   return (
     <div>
       <Navbar />
-      <Scrap_product />
+
+      {userInfo.like_item_count === '0' ? <List_Null_Scrap /> : <Scrap_product />}
     </div>
   );
 }
