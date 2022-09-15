ac941eb9483d967944896c9d1d2123f5e90b7831
zkxmdkdltm <zkxmdkdltm@gmail.com>
2022-09-16 01:06:02 +0900

    refactor: item & filtered item list & api, feat: pagination package

--- trashion_front/src/api/itemApi.js
+++ trashion_front/src/api/itemApi.js
@@ -8,6 +8,9 @@ export default {
   getProductInfo(item_id) {
     return axios.get('/item_post/item/' + item_id);
   },
+  getProduct() {
+    return axios.get('/item_post/item');
+  },
   delete(id) {
     return axios.delete(`/item_post/item/${id}`, tokenConfig());
   },
@@ -16,5 +19,16 @@ export default {
   },
   getMyItem(id) {
     return axios.get(`/item_post/item/my_item/?user_id=${id}`);
+  },
+  getfilteredItem(cityId, guId, dongId, bigCategory, smallCategory) {
+    if (bigCategory && smallCategory)
+      return axios.get(
+        `item_post/item/?&location_item_sets__location_id__city=${cityId}&location_item_sets__location_id__gu=${guId}&location_item_sets__location_id__dong=${dongId}&category_id__big_category=${bigCategory}&category_id__small_category=${smallCategory}`,
+      );
+    else if (bigCategory && !smallCategory)
+      return axios.get(
+        `item_post/item/?&location_item_sets__location_id__city=${cityId}&location_item_sets__location_id__gu=${guId}&location_item_sets__location_id__dong=${dongId}&category_id__big_category=${bigCategory}`,
+      );
+    else return axios.get(`item_post/item/?&location_item_sets__location_id__city=${cityId}&location_item_sets__location_id__gu=${guId}&location_item_sets__location_id__dong=${dongId}`);
   },
 };
--- trashion_front/src/pages/Product_detail/Product_detail.js
+++ trashion_front/src/pages/Product_detail/Product_detail.js
@@ -47,11 +47,13 @@ const Product_detail = () => {
         seller_height: res.data.seller_height,
         seller_weight: res.data.seller_weight,
       });
+      console.log(res);
 
-      setCityName(sido.filter((el) => el.sido === res.data.locationSet[0].location.city)[0]?.codeNm);
-      setGuName(sigugun.filter((el) => el.sido === res.data.locationSet[0].location.city && el.sigugun === res.data.locationSet[0].location.gu)[0]?.codeNm);
+      setCityName(sido.filter((el) => el.sido === res.data.location_sets[0].location.city)[0]?.codeNm);
+      setGuName(sigugun.filter((el) => el.sido === res.data.location_sets[0].location.city && el.sigugun === res.data.location_sets[0].location.gu)[0]?.codeNm);
       setDongName(
-        dong.filter((el) => el.sido === res.data.locationSet[0].location.city && el.sigugun === res.data.locationSet[0].location.gu && el.dong === res.data.locationSet[0].location.dong)[0]?.codeNm,
+        dong.filter((el) => el.sido === res.data.location_sets[0].location.city && el.sigugun === res.data.location_sets[0].location.gu && el.dong === res.data.location_sets[0].location.dong)[0]
+          ?.codeNm,
       );
     });
   };
